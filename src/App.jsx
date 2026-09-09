0d2d4204f3b361afdc428e895fc2ffa558d28722
JuanB0t <juan.f.santos6@ba.estudante.senai.br>
2026-09-09 13:12:53 -0300

    Base Inicial Do vite configurada

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>TechNexus - Projeto Configurado!</h1>
+      <p>O Vite encontrou o arquivo e a aplicação está rodando perfeitamente.</p>
+    </div>
+  )
+}
