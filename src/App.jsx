f4be5a47f3fca289c40e65214d817b1594077ebe
JuanB0t <juan.f.santos6@ba.estudante.senai.br>
2026-09-09 13:57:14 -0300

    Atualiza Login Na Estrutura

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import React from 'react'
+import React from 'react';
+import AppRoutes from './routes/appRoutes';
 
 export default function App() {
-  return (
-    <div style={{ padding: '2rem', textAlign: 'center', fontFamily: 'sans-serif' }}>
-      <h1>TechNexus - Projeto Configurado!</h1>
-      <p>O Vite encontrou o arquivo e a aplicação está rodando perfeitamente.</p>
-    </div>
-  )
+  return <AppRoutes />;
 }
