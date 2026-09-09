1ca87bfdecdcc3de974acfa41c779ff04ad3e1dc
JuanB0t <juan.f.santos6@ba.estudante.senai.br>
2026-09-09 19:27:21 -0300

    Adiciona Logo as paginas

--- src/componentes/Logo/logo.jsx
+++ src/componentes/Logo/logo.jsx
@@ -1,47 +1,11 @@
 import React from 'react';
+import logoImg from '../../assets/logo.jpg';
 import './logo.css';
 
 export default function Logo({ variant = 'full' }) {
   return (
     <div className="technexus-logo">
-      <div className="logo-icon-wrapper">
-        <svg
-          width="28"
-          height="28"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M12 2L2 7L12 12L22 7L12 2Z"
-            stroke="url(#logo-grad)"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M2 17L12 22L22 17"
-            stroke="url(#logo-grad)"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M2 12L12 17L22 12"
-            stroke="url(#logo-grad)"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <defs>
-            <linearGradient id="logo-grad" x1="2" y1="2" x2="22" y2="22" gradientUnits="userSpaceOnUse">
-              <stop stopColor="#c084fc" />
-              <stop offset="1" stopColor="#6366f1" />
-            </linearGradient>
-          </defs>
-        </svg>
-      </div>
-
+      <img src={logoImg} alt="TechNexus Logo" className="logo-image" />
       {variant === 'full' && (
         <div className="logo-text-group">
           <span className="brand-name">TechNexus</span>
