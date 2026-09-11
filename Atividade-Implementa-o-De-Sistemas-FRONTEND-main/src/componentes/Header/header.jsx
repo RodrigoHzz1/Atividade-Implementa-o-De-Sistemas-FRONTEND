74aad186c6a42dcbb9d7997f68547ca501bad78c
JuanB0t <juan.f.santos6@ba.estudante.senai.br>
2026-09-11 20:48:29 -0300

    Ajustes no Header

--- Atividade-Implementa-o-De-Sistemas-FRONTEND-main/src/componentes/Header/header.jsx
+++ Atividade-Implementa-o-De-Sistemas-FRONTEND-main/src/componentes/Header/header.jsx
@@ -3,17 +3,20 @@ import Logo from '../Logo/logo';
 import './header.css';
 
 export default function Header({ user, onLogout }) {
+  // Pega as propriedades do objeto 'user' ou aplica os valores padrão do sistema
+  const nomeExibicao = user?.nome || 'Carlos Oliveira';
+  const cargoExibicao = user?.cargo || user?.nivel || 'Técnico Nível 1';
+
   return (
     <header className="app-header">
       <Logo variant="full" />
       
       <div className="header-actions">
-        {user && (
-          <div className="user-profile">
-            <span className="user-name">{user.nome}</span>
-            <span className="user-role">{user.cargo}</span>
-          </div>
-        )}
+        <div className="user-profile">
+          <span className="user-name">{nomeExibicao}</span>
+          <span className="user-role">{cargoExibicao}</span>
+        </div>
+
         <button onClick={onLogout} className="btn-logout">
           Sair
         </button>
